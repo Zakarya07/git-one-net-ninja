9fb3aa23f93cf7de59034242dd3a2e10f7a03cd0
Zakarya <zakarya.elkhattabi@gmail.com>
2021-06-03 13:50:16 +0200

    Update of feature-2 (hover on links)

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let header = document.querySelector("header");
+const message =document.querySelector("p.message");
 
-header.addEventListener("mouseenter",()=>{
-    alert("entered");
+let links = document.querySelectorAll("nav ul li a");
+
+
+
+
+links.forEach(link=>{
+    console.log(link);
+    link.addEventListener("mouseenter",()=>{
+        message.innerHTML=`You are on the <span> ${link.textContent}</span>`;
+        message.classList.add("test");
+    })
+    link.addEventListener("mouseleave",()=>{
+        message.textContent="";
+        message.classList.remove("test");
+    })
 })
-
-
